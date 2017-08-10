177e57c82e51a7e40ee818470ae9de4287d3bc3c
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-08-10 14:40:31 +0200

    Implement minimum and maximum in actions

--- src/ui/actions.h
+++ src/ui/actions.h
@@ -111,12 +111,20 @@ namespace LCD_UI {
             case KEY_NONE:
                 break;
             case KEY_PLUS:
-                var++;
+                if ((var + step) > maximum)
+                    var = maximum;
+                else
+                    var += step;
+
                 if (instant_update && var_ptr != NULL)
                     *var_ptr = var;
                 break;
             case KEY_MINUS:
-                var--;
+                if ((var - step) < minimum)
+                    var = minimum;
+                else
+                    var -= step;
+
                 if (instant_update && var_ptr != NULL)
                     *var_ptr = var;
                 break;
