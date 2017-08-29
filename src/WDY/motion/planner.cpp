c9fe651acde676a5aaebefe1cfa5802a80ac8522
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-08-29 14:26:54 +0200

    Add accel/decel init values and checks

--- src/WDY/motion/planner.cpp
+++ src/WDY/motion/planner.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "planner.h"
+#include "config.h"
 
 
 using namespace WDY_motion;
@@ -16,6 +17,9 @@ Planner::Planner() {
 
     position_tgt = 0.0;
     position_cur = 0.0;
+
+    accel = WDY_MIN_ACCEL;
+    decel = WDY_MIN_DECEL;
 
     velocity_cur = 0.0;
 
@@ -31,6 +35,8 @@ Move* Planner::plan(float _position, float _speed) {
     _position = (_position < WDY_MIN_POSITION) ? WDY_MIN_POSITION : _position;
     _speed = (_speed > WDY_MAX_SPEED) ? WDY_MAX_SPEED : _speed;
     _speed = (_speed < WDY_MIN_SPEED) ? WDY_MIN_SPEED : _speed;
+    this->accel = (this->accel > WDY_MAX_ACCEL) ? WDY_MAX_ACCEL : this->accel;
+    this->decel = (this->decel < WDY_MIN_DECEL) ? WDY_MIN_DECEL : this->decel;
 
     _plan(_position, _speed, this->accel, this->decel);
     return this->move_cur;
@@ -43,7 +49,7 @@ move_cmd_t Planner::get_next_interval() {
 
     float distance_cur = this->get_distance();
 
-    if (distance_cur == 0) {
+    if (distance_cur == 0 || this->accel <= 0.0 || this->decel <= 0.0) {
         iter_cur++;
         move.velocity = 0.0;
         return move;
